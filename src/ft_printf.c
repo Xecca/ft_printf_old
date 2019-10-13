ab40172fa63c7468fd3cab631f4186b94e3db6e4
Dreik <dreik@Dreiks-Macbook.local>
2019-10-13 21:11:58 +0300

    i have idea

--- src/ft_printf.c
+++ src/ft_printf.c
@@ -6,15 +6,58 @@
 /*   By: dreik <dreik@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/26 18:57:45 by dreik             #+#    #+#             */
-/*   Updated: 2019/10/07 11:17:41 by dreik            ###   ########.fr       */
+/*   Updated: 2019/10/13 09:57:20 by dreik            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
+void minprintf(char *fmt, ...)
+{
+    va_list ар;
+    char *p;
+	char *sval;
+    int ival;
+    double dval;
+	
+    va_start(ap, fmt);
+    for (p = fmt; *р; р++) 
+    {
+        if (*p != '%') 
+        {
+            putchar(*p);
+    		continue;
+		}
+    	switch (*++р) 
+		{
+            case 'd':
+                ival = va_arg(ap, int);
+                printf ("%d", ival);
+                break;
+            case 'f':
+                dval = va_arg(ap, double);
+                printf("%f", dval);
+                break;
+            case 's':
+                for (sval = va_arg(ap, char *); *sval; sval++)
+                putchar(*sval);
+                break;
+            default:
+                putchar(*p);
+    	break;
+		}
+	}
+    va_end(ap); /* очистка, когда все сделано */
+}
+
 int main()
 {
-    
+	char *str = "sdfsfsd \n";
+	char *str2;
+	int i = 10;
+	int j = 5;
+	minprintf(str, i);
+	printf(str, i);
 
     return (0);
 }
--- src/ft_printf.h
+++ src/ft_printf.h
@@ -6,16 +6,22 @@
 /*   By: dreik <dreik@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/01 18:57:59 by dreik             #+#    #+#             */
-/*   Updated: 2019/10/01 19:01:39 by dreik            ###   ########.fr       */
+/*   Updated: 2019/10/13 09:53:27 by dreik            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
-# include "libft/libft.h"
+# include "../libft/libft.h"
 # include <stdio.h>
+# include <stdarg.h>
 
+// typedef struct	va_list
+// {
+//     void		*ap;
+// 	char		*fmt;
+// }				va_start;
 
 
 #endif
